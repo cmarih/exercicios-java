09ac44d1ded533c8ccdd9f745a3ec94a6e985f53
cmarih <marycostajc@gmail.com>
2022-08-17 22:31:06 -0300

    Conversão de °C para °F.

---
+++ src/conversaoFahrenheit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class conversaoFahrenheit {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informa a temperatura em °F: ");
+        int celsius = entrada.nextInt();
+
+        int tempF = (celsius * 9/5) + 32;
+
+        System.out.printf("%d°F = %d°C", celsius, tempF);
+    }
+}
